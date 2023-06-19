564fb90d94dbadbf2b47ceba90e70efec3540efb
ach2k <ach.2000.b@gmail.com>
2023-06-19 13:24:28 +0200

    beginning

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaHome } from 'react-icons/fa';
+
+
+const Home = () => {
+  return (
+    <>
+    <div>
+    <ul><FaHome color="red"/>
+      </ul>
+    <ul>Lessons
+      </ul>
+      <ul>Exercices
+      </ul>
+      <ul>Youtube
+      </ul>
+      <ul>Dictionary
+      </ul>
+      </div>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaHome } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+    <ul><FaHome color="red"/>
+      </ul>
+    <ul>Lessons
+      </ul>
+      <ul>Exercices
+      </ul>
+      <ul>Youtube
+      </ul>
+      <ul>Dictionary
+      </ul>
+      </div>
+    
+    </>
+  )
+}
+
+export default Navbar
